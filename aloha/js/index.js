4a132317ccad49c8bab4618c33ed9b2ddb6445ea
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2021-01-01 20:56:31 +0800

    110.01.01
    新增JS，順便修正翔暉壞掉的頁面

--- aloha/js/index.js
+++ aloha/js/index.js
@@ -1,3 +1,60 @@
+// home slider start
+var slider_count = 0;
+$("#slider_perv").click(function(){
+	if(	slider_count == 0) return
+	slider_count--
+	$("#slider_box_item").css("transform","translateX(-"+slider_count*100+"%)");
+})
+$("#slider_next").click(function(){	
+	if(	slider_count == 2) return
+	slider_count++
+	$("#slider_box_item").css("transform","translateX(-"+slider_count*100+"%)");
+})
+// home slider end
+
+// search start
+var search_state = false
+$("#search_box_width .search_box_item").click(function(){
+	$(this).toggleClass("active");
+	if($(this).index() == 0){
+		return false;
+	}
+})
+
+$("#active_item li").click(function(){
+	$("#active_item li").removeClass("on");
+	$(this).addClass("on");
+})
+
+
+
+// search end
+// memeber book start
+$("#booking_box_menu li").click(function(){
+	$("#booking_box_menu li").removeClass("on")
+	$(this).addClass("on");
+})
+// memeber book end
+
+
+// detail start
+$("#detail_add .room_amount_num i").click(function(){
+	$("#confirm").addClass("active");
+})
+$("#detail_cancel").click(function(){
+	$("#confirm").removeClass("active");
+})
+
+// detail end
+
+
+
+
+
+
+
+
+
 $("#result_list_header li").click(function () {
 	$("#result_list_header li").removeClass("on");
 	$(this).addClass("on");
@@ -14,20 +71,4 @@ $("#result_close").click(function () {
 })
 
 
-// 搜尋
-$("#search_box_width1").click(function(){
-	$("#search_box_width1").toggleClass('active');
-	return false;
-})
-$("#search_box_width2").click(function(){
-	$("#search_box_width2").toggleClass('active');
-})
-$("#search_box_width3").click(function(){
-	$("#search_box_width3").toggleClass('active');
-})
 
-$("#active_item > li").click(function(){
-	$("#active_item > li").removeClass("on");
-	$(this).addClass("on");
-})
-// 搜尋
